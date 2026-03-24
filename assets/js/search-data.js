80c8c880d94ccbabbd289b8b6736087ad3df2a45
mlkbsalah <96469445+mlkbsalah@users.noreply.github.com>
2026-03-24 19:19:37 +0000

    Deploying to gh-pages from @ mlkbsalah/mlkbsalah.github.io@08d5c331938482b190515b3964bc4a03eafd5e09 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -469,42 +469,28 @@ ninja.data = [{
         title: 'CV',
         section: 'Socials',
         handler: () => {
-          window.open("/assets/pdf/example_pdf.pdf", "_blank");
+          window.open("/assets/pdf/cv.pdf", "_blank");
         },
       },{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%6D%61%6C%65%6B.%62%65%6E-%73%61%6C%61%68@%63%65%6E%74%72%61%6C%65%73%75%70%65%6C%65%63.%66%72", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-linkedin',
+        title: 'LinkedIn',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://www.linkedin.com/in/malek-ben-salah-31b8241b0", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
+        id: 'social-github',
+        title: 'GitHub',
         section: 'Socials',
         handler: () => {
-          window.open("/feed.xml", "_blank");
-        },
-      },{
-        id: 'social-scholar',
-        title: 'Google Scholar',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://github.com/mlkbsalah", "_blank");
         },
       },{
       id: 'light-theme',
